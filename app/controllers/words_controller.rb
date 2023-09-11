888ae2346894266d6b0b110afaf2ecd2af5b176f
hatomugi21 <hatomugi.21@gmail.com>
2023-09-11 18:05:48 +0900

    Active Storageの導入

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,4 +6,9 @@ class WordsController < ApplicationController
   def new
   end
 
+  private
+
+  def word_params
+    params.require(:word).permit(:image).merge(word_id: current_word.id)
+  end
 end
