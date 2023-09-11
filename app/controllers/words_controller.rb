84569cfe421482ab249b9ad6d38c313a181bbf43
hatomugi21 <hatomugi.21@gmail.com>
2023-09-11 19:52:37 +0900

    しりとり単語登録の実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,14 +1,20 @@
 class WordsController < ApplicationController
-
-  def index
+  def new
+    @word = Word.new
   end
 
-  def new
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to @word
+    else
+      render 'new'
+    end
   end
 
   private
 
   def word_params
-    params.require(:word).permit(:image).merge(word_id: current_word.id)
+    params.require(:word).permit(:name, :name_kana, :back_name1, :back_name1_kana, :back_name2, :back_name2_kana, :back_name3, :back_name3_kana, :back_name4, :back_name4_kana, :image)
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,4 @@
 class Word < ApplicationRecord
   has_one_attached :image
+
 end
