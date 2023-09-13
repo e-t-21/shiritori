08e0a01b3dacd67f8752dcb71a7fed9cef3e66e5
hatomugi21 <hatomugi.21@gmail.com>
2023-09-13 12:34:00 +0900

    しりとりゲーム画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'games#index'
-  resources :games
+  resources :games, only: [:index, :play]
   resources :words
+  get 'games/play', to: 'games#play', as: 'play_game'
 end
